8bc99f9e500c432aac341b8b082f8785e867fb9b
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-28 11:36:52 -0400

    Exercise 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,20 @@
+//src/components/App.js
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import styled from "styled-components";
+
 const App = (props) => {
-    return 'Hello world!';
+    return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<h2>Home</h2>} />
+          <Route path="/about" element={<h2>About</h2>} />
+        </Routes>
+    </BrowserRouter>
+    );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+//src/components/Header.js
+
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <Logo> Fruit Emporium</Logo>
+      <Nav>
+        <NavLinkItem to="/"> Home </NavLinkItem>
+        <NavLinkItem to="/about"> About</NavLinkItem>
+      </Nav>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
+
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+
+`;
+
+const Logo = styled.h1`
+  font-size: 24px;
+  margin: 0;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  padding: 5px;
+
+`;
+
+const NavLinkItem = styled(NavLink)`
+  text-decoration: none;
+  margin: 10px 10px;
+  font-size: 16px;
+`;
